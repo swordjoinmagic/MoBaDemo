1d77b5b7885b8234637c9a61a443ad149faafd48
swordjoinmagic <2483787753@qq.com>
2018-10-31 13:46:49 +0800

    小地图功能Demo

--- Scripts/UI/View/MapView.cs
+++ Scripts/UI/View/MapView.cs
@@ -9,13 +9,32 @@ using UnityEngine.EventSystems;
 public class MapView : MonoBehaviour {
 
     private EventTrigger eventTrigger;
+    private Vector2 position;
+    public RectTransform rectTransform;
+    public Camera UICamera;
+    public RenderTexture renderTexture;
+
+    private Vector2 UIToTex;
+    private Vector2 TexToWorld;
+    // 用于修正因为相机视角带来的摄像机距离理想位置的偏移
+    public Vector2 cameraOffset = new Vector2(7f,15f);
 
     private void Start() {
+        // 从UI坐标到贴图坐标的转换
+        UIToTex = new Vector2(256 / rectTransform.sizeDelta.x, 256 / rectTransform.sizeDelta.y);
+        // 从贴图坐标到世界坐标的转换
+        TexToWorld = new Vector2((300f) / 256f, (300f )/ 256f);
+
         eventTrigger = GetComponent<EventTrigger>();
         var clickEntry = new EventTrigger.Entry();
         clickEntry.eventID = EventTriggerType.PointerClick;
         clickEntry.callback.AddListener(eventdata => {
-            
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform,Input.mousePosition,UICamera,out position);
+            Vector2 cameraWorldPosition = TexToWorld * (UIToTex * position);
+            Camera.main.transform.position = new Vector3(cameraWorldPosition.x-cameraOffset.x,Camera.main.transform.position.y,cameraWorldPosition.y-cameraOffset.y);
+            Debug.Log("cameraWorldPosition:"+cameraWorldPosition);
         });
+        eventTrigger.triggers.Add(clickEntry);
+
     }
 }
---
+++ Scripts/UITest/MapTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class MapTest : MonoBehaviour, IPointerClickHandler {
+
+    //监听点击
+    public void OnPointerClick(PointerEventData eventData) {
+        PassEvent(eventData, ExecuteEvents.pointerClickHandler);
+    }
+
+
+    //把事件透下去
+    public void PassEvent<T>(PointerEventData data, ExecuteEvents.EventFunction<T> function)
+        where T : IEventSystemHandler {
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(data, results);
+        GameObject current = data.pointerCurrentRaycast.gameObject;
+        Debug.Log("ResultsCount:"+results.Count);
+        for (int i = 0; i < results.Count; i++) {
+            if (current != results[i].gameObject) {
+                ExecuteEvents.Execute(results[i].gameObject, data, function);
+                //RaycastAll后ugui会自己排序，如果你只想响应透下去的最近的一个响应，这里ExecuteEvents.Execute后直接break就行。
+            }
+        }
+    }
+
+
+}
